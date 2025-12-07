980e30b24b49d8a89ae17329233d544a8b27ff4f
Dylan <dylanzfernandes@gmail.com>
2025-12-07 22:02:53 +0530

    env support

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
 import express from "express";
 
-const port = 3000;
+dotenv.config()
+
+const port = process.env.PORT ||3000;
 const app = express();
 
 app.use(express.json());
